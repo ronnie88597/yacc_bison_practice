9fb33efae002d92d6c023c2ea10ced7635a0c944
ronnie88597 <897045953@qq.com>
2020-04-26 23:28:28 +0800

    add

---
+++ cmake-build-debug/ch2/CMakeFiles/2.02_use_yyin_to_read_data_from_file.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "2.02_use_yyin_to_read_data_from_file"
+  "2.02_use_yyin_to_read_data_from_file.pdb"
+  "CMakeFiles/2.02_use_yyin_to_read_data_from_file.dir/2.02_use_yyin_to_read_data_from_file.c.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/2.02_use_yyin_to_read_data_from_file.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
